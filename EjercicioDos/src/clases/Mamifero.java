e2fa309bee6ad6a469a94b89323370aef45b82e7
Juan Felipe Muñoz Hurtado <juanfelipemh@gmail.com>
2023-01-26 09:44:53 -0500

    Solution

--- EjercicioDos/src/clases/Mamifero.java
+++
@@ -1,8 +0,0 @@
-package clases;
-
-public class Mamifero extends Animal {
-    
-    public String agruparManda(int cantidad){
-        return "La manada se agrupa y en total hay " + cantidad + " animales";
-    }
-}
--- EjercicioDos/src/interfaces/Muerte.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-public interface Muerte {
-    
-    public String morir(int anoNacimiento, int anoFallecimiento, String causaMuerte);
-    
-    public String reencarnar(String dato);
-    
-}
