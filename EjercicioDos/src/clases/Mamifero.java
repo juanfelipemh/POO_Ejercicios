5c16ac2ac922a2229430a12777e245b7d2968330
Juan Felipe Muñoz Hurtado <juanfelipemh@gmail.com>
2023-01-26 10:12:16 -0500

    Correct commit with correct modules

---
+++ EjercicioDos/src/clases/Mamifero.java
@@ -0,0 +1,8 @@
+package clases;
+
+public class Mamifero extends Animal {
+    
+    public String agruparManda(int cantidad){
+        return "La manada se agrupa y en total hay " + cantidad + " animales";
+    }
+}
